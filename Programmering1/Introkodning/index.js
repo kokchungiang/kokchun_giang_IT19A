7328e44f65eda7bbbafbd4fe7161cf314721181f
Kokchun <69901510+kokchungiang@users.noreply.github.com>
2020-09-01 09:40:02 +0200

    error med DOM

--- Programmering1/Introkodning/index.js
+++ Programmering1/Introkodning/index.js
@@ -1,5 +1,17 @@
 
 // skapar en variabel som heter name och tilldelar det värdet "Kokchun"
-let name = "Kokchun"
+let name = "Kokchun" // sträng
+let age = 16 // heltal, int 
 
-console.log(`Hej ${name}`)
+console.log(`Hej ${name} du är ${age} år gammal`)
+
+// if-sats - att göra val
+if (age >= 18){
+    console.log("Du är vuxen")
+} else {
+    console.log("Du är barn")
+}
+
+// DOM - Document Object Model
+let text = document.querySelector("#text")
+text.innerHTML = `${name} är ${age} år gammal`
